2efc277b07c5e9f66106d385eff09ca3ff4b664e
drupalmk <leisikkeram@gmail.com>
2011-10-24 13:40:03 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e9e3c549eb2f0130204077390101e73ac1ccbdcd299df08b29aea69b2db0504ea2b69875adac20b27955a4cfae32ebfef80797370d44cc9737dbf24b7e7347a0'
